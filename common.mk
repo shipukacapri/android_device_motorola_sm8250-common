805bc4ee45e879211d6b193636cf43521fbeba6f
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-02-17 02:14:30 +0530

    infinity bringup

--- common.mk
+++ common.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common InfinityX stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
