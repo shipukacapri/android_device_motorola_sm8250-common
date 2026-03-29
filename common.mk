f88d493219b591924b36a1984cdc6f09a1a53101
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-03-29 13:47:11 +0530

    inherit dolby

--- common.mk
+++ common.mk
@@ -181,6 +181,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Authsecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
